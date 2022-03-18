3d4016169aefc5fb457eb256f4b9920a13353ae5
Gael Duong <mr_jupipro@yahoo.com>
2022-03-18 03:27:24 -0400

    Set up dynamic route on product lcick

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -38,6 +38,7 @@ const ProductList = () => {
     return (
       <ProductCard
         key={product.id}
+        id={product.id}
         image={product.images[0]}
         name={product.name}
         price={{
