00eacc973a438c3a05294979faaf9132f93be1a3
Gael Duong <mr_jupipro@yahoo.com>
2022-04-23 02:14:58 -0400

    Refactor Navbar

--- src/layouts/Navbar/Navbar.jsx
+++ src/layouts/Navbar/Navbar.jsx
@@ -3,6 +3,17 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Link, useLocation } from 'react-router-dom'
 import { removeCurrentUser } from 'redux/slices/userSlice'
 
+const {
+  SIGN_UP,
+  LOGIN,
+  HOME,
+  CHECKOUT,
+  PAYMENT,
+  ORDERS,
+  USER_INFO,
+  NOT_FOUND
+} = PATH
+
 const Navbar = () => {
   const { pathname } = useLocation()
   const { currentUser } = useSelector(state => state.currentUser)
@@ -15,23 +26,23 @@ const Navbar = () => {
   return (
     <nav>
       <h1>
-        <Link to={PATH.SIGN_UP}>Sign up</Link>
+        <Link to={SIGN_UP}>Sign up</Link>
         {' | '}
-        <Link to={PATH.LOGIN} state={{ previousPath: pathname }}>
+        <Link to={LOGIN} state={{ previousPath: pathname }}>
           Login
         </Link>
         {' | '}
-        <Link to={PATH.HOME}>ProductSearch </Link>
+        <Link to={HOME}>ProductSearch </Link>
         {' | '}
-        <Link to={PATH.CHECKOUT}>Checkout</Link>
+        <Link to={CHECKOUT}>Checkout</Link>
         {' | '}
-        <Link to={PATH.PAYMENT}>Payment</Link>
+        <Link to={PAYMENT}>Payment</Link>
         {' | '}
-        <Link to={PATH.ORDERS}>UserOrders</Link>
+        <Link to={ORDERS}>UserOrders</Link>
         {' | '}
-        <Link to={PATH.USER_INFO}>UserInfo</Link>
+        <Link to={USER_INFO}>UserInfo</Link>
         {' | '}
-        <Link to={PATH.NOT_FOUND}>Not Found</Link>
+        <Link to={NOT_FOUND}>Not Found</Link>
       </h1>
       {currentUser && <button onClick={handleLogout}>Logout</button>}
     </nav>
